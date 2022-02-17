c0d7b86c5860c45b0840cafafed425727e96d2fe
David Paulsson <david@davidpaulsson.se>
2022-02-17 14:21:34 +0100

    feat: hide app from dock

--- main.js
+++ main.js
@@ -6,8 +6,16 @@ const isDev = process.env.NODE_ENV === 'development';
 const mb = menubar({
   tooltip: 'Hem Wallpapers',
   icon: path.join(__dirname, 'assets/icon.png'),
+  showDockIcon: false,
+  preloadWindow: true,
+  skipTaskbar: true,
   browserWindow: {
+    movable: false,
+    minimizable: false,
+    maximizable: false,
     alwaysOnTop: isDev,
+    showDockIcon: false,
+    preloadWindow: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
@@ -16,4 +24,19 @@ const mb = menubar({
   },
 });
 
-mb.on('ready', () => console.log('app is ready'));
+mb.on('ready', () => {
+  console.log('app is ready');
+});
+
+mb.on('after-create-window', () => {
+  // hide dock icon
+  if (process.platform === 'darwin') {
+    mb.app.dock.hide();
+  }
+});
+
+// disable backgrounding the app to avoid a flash when opening your menubar app
+mb.app.commandLine.appendSwitch(
+  'disable-backgrounding-occluded-windows',
+  'true',
+);

f25ce363905b7d34a00dc16cb10648079649a440
David Paulsson <david@davidpaulsson.se>
2022-02-17 12:25:12 +0100

    feat: first draft version

--- preload.js
+++ preload.js
@@ -11,14 +11,61 @@ window.addEventListener('DOMContentLoaded', () => {
 
 const { contextBridge } = require('electron');
 const wallpaper = require('wallpaper');
+const contentful = require('contentful');
+const os = require('os');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = fs.promises;
+
+const contentfulClient = contentful.createClient({
+  space: '63ifdrmbhu32',
+  accessToken: 'Ny-t2TIWpKZfsntKbEZv7dvLZmTCOEFpnweIfyPR8t0',
+});
+
+const getBase64Image = async (url) => {
+  const response = await fetch(url);
+  const blob = await response.blob();
+  const reader = new FileReader();
+  await new Promise((resolve, reject) => {
+    reader.onload = resolve;
+    reader.onerror = reject;
+    reader.readAsDataURL(blob);
+  });
+  return reader.result.replace(/^data:.+;base64,/, '');
+};
 
 contextBridge.exposeInMainWorld('electron', {
   getCurrentWallpaper: async () => {
     const currentWallpaper = await wallpaper.get();
     console.log('current wallpaper', currentWallpaper);
   },
-  setCurrentWallpaper: async (path) => {
-    await wallpaper.set(path);
-    console.log('current wallpaper', path);
+  setCurrentWallpaper: async (url) => {
+    try {
+      const base64Image = await getBase64Image(url);
+      const picturePath = path.join(
+        os.homedir(),
+        '/Pictures',
+        'hem-wallpaper.jpg',
+      );
+
+      await fsPromises.writeFile(
+        path.normalize(picturePath),
+        base64Image,
+        'base64',
+      );
+      await wallpaper.set(picturePath);
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  getRandomImage: async () => {
+    try {
+      const response = await contentfulClient.getAssets();
+      const itemsLength = response.items.length;
+      const randomIndex = Math.floor(Math.random() * itemsLength);
+      return response.items[randomIndex].fields.file.url;
+    } catch (err) {
+      console.error(err);
+    }
   },
 });
--- renderer.js
+++ renderer.js
@@ -1,11 +1,66 @@
-console.log('hello!!');
+const currentImage = document.getElementById('current-image');
+const swapCurrentImage = document.getElementById('swap-current-image');
+const setAsWallpaper = document.getElementById('set-as-wallpaper');
+const iconRefresh = document.querySelector('.image__refresh');
+const iconSpinner = document.querySelector('.image__spinner');
 
-window.electron.getCurrentWallpaper();
+const imageFormatParams = '?w=400&fm=avif';
 
-// on click event handler for #set-as-wallpaper
-document.getElementById('set-as-wallpaper').addEventListener('click', () => {
-  console.log('clicked!');
-  window.electron.setCurrentWallpaper(
-    './assets/2022_Feb_Hem_HQ_Erik_Lefvander_2718.jpg',
-  );
+function getRandomImageUrl() {
+  return new Promise((resolve) => {
+    window.electron.getRandomImage().then((url) => {
+      resolve('https:' + url);
+    });
+  });
+}
+
+function updatePreviewImage(url) {
+  currentImage.src = url + imageFormatParams;
+}
+
+swapCurrentImage.addEventListener('click', async () => {
+  iconRefresh.style.display = 'none';
+  iconSpinner.style.display = 'block';
+  const url = await getRandomImageUrl();
+  updatePreviewImage(url);
 });
+
+currentImage.onload = () => {
+  iconRefresh.style.display = 'block';
+  iconSpinner.style.display = 'none';
+  console.group('loaded!');
+};
+
+const successMessages = [
+  'Great choice! ✨',
+  'Nice! 🤩',
+  'High fives!',
+  'You got it! 🎉',
+  'Hooray! You did it!',
+  'Looking crisp!',
+];
+
+setAsWallpaper.addEventListener('click', async () => {
+  setAsWallpaper.innerText = 'Setting wallpaper…';
+  setAsWallpaper.disabled = true;
+
+  const url = currentImage.src.replace(imageFormatParams, '');
+  await window.electron.setCurrentWallpaper(url);
+
+  // get random successMessage
+  const randomIndex = Math.floor(Math.random() * successMessages.length);
+  setAsWallpaper.innerText = successMessages[randomIndex];
+
+  setTimeout(() => {
+    setAsWallpaper.innerText = 'Set as wallpaper';
+    setAsWallpaper.disabled = false;
+  }, 2000);
+});
+
+// on app load
+(async () => {
+  iconRefresh.style.display = 'none';
+  iconSpinner.style.display = 'block';
+  const url = await getRandomImageUrl();
+  updatePreviewImage(url);
+})();
